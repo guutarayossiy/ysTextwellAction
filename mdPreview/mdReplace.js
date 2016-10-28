10e049040fd32eae52cddde7bfbfda5972a197d8
Yoshiaki Ogata <yosi@yosi.local>
2016-10-29 06:59:29 +0900

    Fix - mdreplace dose not work

--- mdPreview/mdReplace.js
+++ mdPreview/mdReplace.js
@@ -1,5 +1,4 @@
-;(function() {
-  function mdreplace(text){
+function mdreplace(text){
     var result = '';
     var BlogTitle = ' – たらハコ';
 
@@ -14,12 +13,11 @@
         .replace(/<\/h2>/g,"</h2>\n")
         .replace(/<h3>/g,"\n<h3>")
         .replace(/<\/h3>/g,"</h3>\n")
-        .replace('<!--more-->',"\n<!--more-->\n")
+        .replace('<!--more-->',"\n<!--more-->")
         .replace(/<pre>/g,"\n<pre>")
-        .replace(/<\/pre>/g,"</pre>\n")
+        .replace(/<\/pre>/g,"</pre>\n\n")
         .replace(/<pre><code>/g,"<pre>")
         .replace(/<\/code><\/pre>/g,"</pre>");
 
     return result;
-  };
-}());
+};
