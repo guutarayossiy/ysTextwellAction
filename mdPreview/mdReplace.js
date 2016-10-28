00877c4aec24743297337eb545e7e662ea79d144
Yoshiaki Ogata <yosi@yosi.local>
2016-10-29 06:48:40 +0900

    Fix - mdPreview js bug

--- mdPreview/mdReplace.js
+++ mdPreview/mdReplace.js
@@ -15,9 +15,9 @@
         .replace(/<h3>/g,"\n<h3>")
         .replace(/<\/h3>/g,"</h3>\n")
         .replace('<!--more-->',"\n<!--more-->\n")
-        .replace(/<pre>/g,"\n<pre>");
-        .replace(/<\/pre>/g,"</pre>\n");
-        .replace(/<pre><code>/g,"<pre>");
+        .replace(/<pre>/g,"\n<pre>")
+        .replace(/<\/pre>/g,"</pre>\n")
+        .replace(/<pre><code>/g,"<pre>")
         .replace(/<\/code><\/pre>/g,"</pre>");
 
     return result;
